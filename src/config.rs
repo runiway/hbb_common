c527d56c603543913581cc224ef4bc5aa61f6bb4
runiway <RYAN@UNIWAY.CA>
2025-03-18 15:19:44 -0600

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["184.70.62.246"];
+pub const RS_PUB_KEY: &str = "F9U65RHUHpS7UDkK81qrkVixh4sy7Em2gQnuzdPzTmE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
